97db1ebb03b9c326612b157f46a9953e9b33ad41
Soramichi Akiyama <akiyama@m.soramichi.jp>
2020-01-12 21:02:54 +0900

    fix comments so that they match the readme

--- delete_old_kernels.py
+++ delete_old_kernels.py
@@ -19,7 +19,7 @@ def newer(v1, v2):
     if len(v1) == len(v2):
         return False
     # if v1 is the same as v2 up to len(v2) and still has something, probable v1 is newer
-    # Example: v1 = "5.4.8.1" vs. v2 = "5.4.8"
+    # Example: v1 = "5.4.10.1" vs. v2 = "5.4.10"
     elif len(v1) > len(v2):
         return True
     # the other way around
@@ -40,7 +40,7 @@ def delete_files(v):
         force_delete(f + v)
 
 if __name__ == "__main__":
-    cur_ver = subprocess.check_output(["uname", "-r"], text=True) # => '5.4.8\n'
+    cur_ver = subprocess.check_output(["uname", "-r"], text=True) # => '5.4.10\n'
     cur_ver = cur_ver[0:-1] # delete the tailing '\n'
 
     installed = []
